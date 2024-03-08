71fd1815c587ec783c7b6146baa5c58f6a256584
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2024-03-08 16:53:45 -0600

    init

---
+++ runnable_extension_editor.js
@@ -0,0 +1,8 @@
+document.body.element.style.border = "thick solid #0000FF";
+var a =1
+if(!document.body.element.style.overflow || a > 0){
+document.body.element.style.overflow = true;} 
+else{ a--;
+console.log("counting down");
+}
+console.log("its over");
