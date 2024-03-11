4467a755e887227369b43e42bfd7ebc4dc80df51
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2024-03-10 22:53:24 -0500

    work in progress

---
+++ PagEdit Red/script.js
@@ -0,0 +1,16 @@
+var button1 = document.getElementById("overflow-button");
+if (button1) button1.addEventListener("click", aler);
+function aler() { alert('a'); }
+
+
+
+
+/*async function fetchData() {
+    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+    const record=await res.json();
+    document.getElementById("date").innerHTML=record.data[0].date;
+    document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
+fetchData();*/
--- in progress/script.js
+++ in progress/script.js
@@ -1,4 +1,19 @@
-async function fetchData() {
+document.body.style.backgroundColor = "yellow";
+var button = document.getElementById("button1");
+button.style.visibility = "visible";
+
+
+
+/*
+var button1 = document.getElementById("overflow-button");
+if (button1) button1.addEventListener("click", aler);
+function aler() { alert('a'); }
+*/
+
+
+
+
+/*async function fetchData() {
     const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
     const record=await res.json();
     document.getElementById("date").innerHTML=record.data[0].date;
@@ -6,4 +21,4 @@ async function fetchData() {
     document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
     document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
 }
-fetchData();
+fetchData();*/
